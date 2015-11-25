b57066bc536090facff3fad9a4508825b237d032
fingerstip <396909730@qq.com>
2015-11-25 18:25:40 +0800

    update

--- src/com/linng/www/controller/WebController.java
+++ src/com/linng/www/controller/WebController.java
@@ -1,26 +1,40 @@
 package com.linng.www.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 
- *
- *<pre>
-    林花谢了春红，
-    太匆匆，
-    无奈朝来寒雨晚来风。
- 
-    胭脂泪，
-    相留醉，
-    几时重，
-    自是人生长恨水长东。
- *</pre>
+ * 
+ * <pre>
+ *     林花谢了春红，
+ *     太匆匆，
+ *     无奈朝来寒雨晚来风。
+ *  
+ *     胭脂泪，
+ *     相留醉，
+ *     几时重，
+ *     自是人生长恨水长东。
+ * </pre>
  * 
  * @author LiNing
- *
+ * 
  */
 @Controller
+@RequestMapping("/view")
 public class WebController {
-	
+
+	@RequestMapping("/hehe")
+	public void index() { }
 
 }
